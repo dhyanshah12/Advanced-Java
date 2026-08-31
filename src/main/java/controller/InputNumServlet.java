96de59e8fbcd7edfc2abb0ddb3e8dc1d1d1fbdfc
dhyanshah12 <dhyanshah70@gmail.com>
2026-08-31 13:15:06 +0530

    special characters

--- src/main/java/controller/InputNumServlet.java
+++ src/main/java/controller/InputNumServlet.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,11 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 public class InputNumServlet extends HttpServlet
 {
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
-		int num1 = Integer.parseInt(req.getParameter("n1"));
-		int num2 = Integer.parseInt(req.getParameter("n2"));
+		String strN1 = req.getParameter("n1");
+		String strN2 = req.getParameter("n2");
+		String opr  =  req.getParameter("opr");
 		
-		System.out.println(num1+num2);
+		int n1 = Integer.parseInt(strN1);
+		int n2 = Integer.parseInt(strN2);
+		
+		PrintWriter  out = res.getWriter(); 
+		if(opr.equals("add"))
+		{
+			int ans = n1+n2;
+			out.print(ans);
+		}else if(opr.equals("sub")) {
+			int ans = n1 - n2;
+			out.print(ans);
+		}
+		else if(opr.equals("mul")) {
+			int ans = n1 * n2;
+			out.print(ans);
+		}
+		else if(opr.equals("div")) {
+			int ans = n1 / n2;
+			out.print(ans);
+		}
 	}
 }
