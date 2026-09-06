f432ab59675bc000ebe0cd6a1197ec88381a10e7
dhyanshah12 <dhyanshah70@gmail.com>
2026-09-06 15:44:19 +0530

    validators

--- src/main/java/controller/InputNameServlet.java
+++ src/main/java/controller/InputNameServlet.java
@@ -21,30 +21,30 @@ public class InputNameServlet extends HttpServlet
 		String email = req.getParameter("email");
 		String Password = req.getParameter("password");
 		boolean isError = false;
-//		String alphaRegEx = "[a-zA-Z]+";
-//		
-//		if(Validators.isBlank(name)) {
-//			isError = true;
-//			req.setAttribute("nameerr","Please Enter Name");
-//		}else if(!Validators.isBlank(name)) {
-//			req.setAttribute("nameError", "Please Enter Valid Name");
-//			req.setAttribute("nameValue",name);
-//		}else {
-//			req.setAttribute("nameValue",name);
-//		}
-//		
-//		
-//		if(Validators.isBlank(email)) {
-//			isError = true;
-//			req.setAttribute("emailerr","Please Enter Email");
-//		}else {
-//			req.setAttribute("emailValue",email);
-//		}
-//		if(Validators.isBlank(Password)) {
-//			isError = true;
-//			req.setAttribute("passerr","Please Enter Password");
-//		}
-//		
+		String alphaRegEx = "[a-zA-Z]+";
+		
+		if(Validators.isBlank(name)) {
+			isError = true;
+			req.setAttribute("nameerr","Please Enter Name");
+		}else if(!Validators.isBlank(name)) {
+			req.setAttribute("nameError", "Please Enter Valid Name");
+			req.setAttribute("nameValue",name);
+		}else {
+			req.setAttribute("nameValue",name);
+		}
+		
+		
+		if(Validators.isBlank(email)) {
+			isError = true;
+			req.setAttribute("emailerr","Please Enter Email");
+		}else {
+			req.setAttribute("emailValue",email);
+		}
+		if(Validators.isBlank(Password)) {
+			isError = true;
+			req.setAttribute("passerr","Please Enter Password");
+		}
+		
 		if(isError) {
 			RequestDispatcher rd = req.getRequestDispatcher("InputName.jsp");
 			rd.forward(req, res);
